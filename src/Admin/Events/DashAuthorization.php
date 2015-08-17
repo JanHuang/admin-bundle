67b9c1317af3256edcc3e46961ae9f8c9db3c082
janhuang <384099566@qq.com>
2015-08-17 21:48:25 +0800

    fix redirect bug

--- src/Admin/Events/DashAuthorization.php
+++ src/Admin/Events/DashAuthorization.php
@@ -38,7 +38,7 @@ class DashAuthorization extends TemplateEvent
                     'msg' => 'Access denied.',
                 ], Response::HTTP_FORBIDDEN);
             }
-            return $this->redirect($this->generateUrl('dash_admin__login'));
+            return $this->redirect($this->generateUrl('dash_admin_login'));
         }
 
         $this->user = $request->getSession('manager');
